01afc65c59696ddf96177048c27c227991a1b23e
TorranceTech <billy.anderson.it@gmail.com>
2024-05-25 05:35:19 -0700

    Create clean_mac_temp_files.sh
    Download the Script:
    Download the script from the GitHub repository.
    
    Save the Script:
    Save the script to your desired location on your macOS system.
    
    Make the Script Executable:
    Open Terminal and navigate to the script's directory. Run the following command to make the script executable:
    
    sh
    Copy code
    chmod +x clean_mac_temp_files.sh
    Run the Script:
    Execute the script with superuser privileges to ensure it can access and clean all necessary directories:
    
    sh
    Copy code
    sudo ./clean_mac_temp_files.sh

---
+++ clean_mac_temp_files.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Defining colors for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Function to check if the directory exists before attempting to clean it
+safe_rm() {
+    if [ -d "$1" ]; then
+        echo -e "${GREEN}Cleaning $1...${NC}"
+        rm -rfv "$1"/*
+    else
+        echo -e "${RED}Directory $1 does not exist and will be ignored.${NC}"
+    fi
+}
+
+echo -n "Removing .DS_Store files..."
+find / -name '.DS_Store' -type f -delete
+echo -e "${GREEN}Done!${NC}"
+
+echo -n "Cleaning cache files..."
+safe_rm ~/Library/Logs/
+safe_rm ~/System/Library/Caches/
+safe_rm ~/Library/Logs/CrashReporter/CoreCapture
+safe_rm ~/Library/Logs/CrashReporter
+safe_rm ~/Library/Logs/Microsoft/InstallLogs
+safe_rm ~/Library/Caches
+safe_rm /Library/Caches
+safe_rm /System/Library/Caches
+safe_rm /Users/$(whoami)/Library/Caches
+echo -e "${GREEN}Done!${NC}"
+
+echo -n "Restarting network services..."
+killall -HUP mDNSResponder
+echo -e "${GREEN}Done!${NC}"
+
+echo -n "Cleaning system logs and crash reports..."
+safe_rm /var/log
+safe_rm ~/Library/Logs
+safe_rm /Library/Logs
+safe_rm /Library/Logs/DiagnosticReports
+echo -e "${GREEN}Done!${NC}"
+
+echo -n "Cleaning temporary files..."
+safe_rm /private/var/folders
+echo -e "${GREEN}Done!${NC}"
+
+echo -n "Removing items from trash..."
+safe_rm ~/.Trash
+echo -e "${GREEN}Done!${NC}"
+
+echo -n "Cleaning Time Machine snapshots..."
+tmutil listlocalsnapshots / | grep 'com.apple.TimeMachine' | while read -r snapshot; do
+    sudo tmutil deletelocalsnapshots "${snapshot##* }"
+done
+echo -e "${GREEN}Done!${NC}"
+
+echo -n "Updating Spotlight index..."
+sudo mdutil -E /
+echo -e "${GREEN}Done!${NC}"
+
+echo -n "Cleaning Dock data sources..."
+killall Dock
+echo -e "${GREEN}Done!${NC}"
+
+# Adding a free space check after cleaning
+FREE_SPACE=$(df -h / | tail -1 | awk '{print $4}')
+echo -e "${GREEN}Free space after cleaning: $FREE_SPACE${NC}"
